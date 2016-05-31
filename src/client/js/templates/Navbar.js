6bf63ef1c7d99c32eac975fbb706b5dcf653f3ec
mrzapp <contact@jeppezapp.com>
2016-05-31 17:33:56 +0200

    corrected navbar links

--- src/client/js/templates/Navbar.js
+++ src/client/js/templates/Navbar.js
@@ -3,19 +3,19 @@
 module.exports = function Navbar() {
     return _.div({class: 'navbar'},
         _.div({class: 'buttons'},
-            _.a({href: '/#/user/', class: Router.url == '/user/' ? 'active' : ''},
+            _.a({href: './#/user/', class: Router.url == '/user/' ? 'active' : ''},
                 _.span({class: 'fa fa-user'})
             ),
-            _.a({href: '/#/plan/', class: Router.url == '/plan/' ? 'active' : ''},
+            _.a({href: './#/plan/', class: Router.url == '/plan/' ? 'active' : ''},
                 _.span({class: 'fa fa-calendar'})
             ),
-            _.a({href: '/#/board/', class: Router.url == '/board/' ? 'active' : ''},
+            _.a({href: './#/board/', class: Router.url == '/board/' ? 'active' : ''},
                 _.span({class: 'fa fa-columns'})
             ),
-            _.a({href: '/#/list/', class: Router.url == '/list/' ? 'active' : ''},
+            _.a({href: './#/list/', class: Router.url == '/list/' ? 'active' : ''},
                 _.span({class: 'fa fa-list'})
             ),
-            _.a({href: '/#/settings/', class: Router.url == '/settings/' ? 'active' : ''},
+            _.a({href: './#/settings/', class: Router.url == '/settings/' ? 'active' : ''},
                 _.span({class: 'fa fa-cog'})
             )
 

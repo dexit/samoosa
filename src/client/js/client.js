679fd2085066d15ae7d383b08e570a3e064a76fd
mrzapp <contact@jeppezapp.com>
2016-11-16 13:24:12 +0100

    remoed babel polyfill

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -10,7 +10,6 @@ window.app = require('../../../package.json');
 require('exomon');
 window.Promise = require('bluebird');
 window.marked = require('marked');
-require('babel-polyfill');
 
 Promise.onPossiblyUnhandledRejection((error, promise) => {
     throw error;
